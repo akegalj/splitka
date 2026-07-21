b4288babce958a3f1e1ea892685a8124f20ecac2
Ante Kegalj <akegalj@gmail.com>
2026-07-21 15:03:45 +0200

    Update scafold

--- Main.hs
+++ Main.hs
@@ -2,27 +2,14 @@
 {-# LANGUAGE LambdaCase #-}
 ----------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 
 ----------------------------------------------------------------------------
 module Main where
 
 ----------------------------------------------------------------------------
 import Miso
+import qualified Miso.Html as H
 import Miso.Lens
-import Miso.String
-
-----------------------------------------------------------------------------
-
--- | Application model state
-newtype Model = Model
-  { _counter :: Int
-  }
-  deriving (Show, Eq)
-
-----------------------------------------------------------------------------
-counter :: Lens Model Int
-counter = lens _counter $ \record field -> record{_counter = field}
 
 ----------------------------------------------------------------------------
 
@@ -33,52 +20,57 @@ data Action
   | SayHelloWorld
   deriving (Show, Eq)
 
+-- NOTE: see https://github.com/haskell-miso/sampler/blob/main/app/Main.hs
+
 ----------------------------------------------------------------------------
 
 -- | Entry point for a miso application
 main :: IO ()
-main = run (startApp app)
-
+#ifdef INTERACTIVE
+main = reload defaultEvents app
+#else
+main = startApp defaultEvents app
+#endif
 ----------------------------------------------------------------------------
 
 -- | WASM export, required when compiling w/ the WASM backend.
 #ifdef WASM
+#ifndef INTERACTIVE
 foreign export javascript "hs_start" main :: IO ()
+#endif
 #endif
 ----------------------------------------------------------------------------
 
--- | `defaultApp` takes as arguments the initial model, update function, view function
-app :: App Model Action
-app = defaultApp emptyModel updateModel viewModel
+-- | `vcomp` takes as arguments the initial model, update function, view function
+app :: App Int Action
+app = vcomp 0 updateModel viewModel
 
-----------------------------------------------------------------------------
-
--- | Empty application state
-emptyModel :: Model
-emptyModel = Model 0
+-- app = (component 0 updateModel viewModel)
+--   { styles = [ Sheet sheet ]
+--   }
 
 ----------------------------------------------------------------------------
 
 -- | Updates model, optionally introduces side effects
-updateModel :: Action -> Effect Model Action
+updateModel :: Action -> Effect parent props Int Action
 updateModel = \case
-  AddOne -> counter += 1
-  SubtractOne -> counter -= 1
-  SayHelloWorld -> io $ do
-    consoleLog "Hello World"
+  AddOne -> this += 1
+  SubtractOne -> this -= 1
+  SayHelloWorld -> io_ $ do
     alert "Hello World"
+    consoleLog "Hello World"
 
 ----------------------------------------------------------------------------
 
 -- | Constructs a virtual DOM from a model
-viewModel :: Model -> View Action
-viewModel x =
-  div_
-    []
-    [ button_ [onClick AddOne] [text "+"]
-    , text $ ms (x ^. counter)
-    , button_ [onClick SubtractOne] [text "-"]
-    , button_ [onClick SayHelloWorld] [text "Alert Hello World!!!!"]
+viewModel :: () -> Int -> View Int Action
+viewModel _props x =
+  vfrag
+    [ H.button_ [H.onClick AddOne] [text "+"]
+    , text (ms x)
+    , H.button_ [H.onClick SubtractOne] [text "-"]
+    , H.br_ []
+    , H.button_ [H.onClick SayHelloWorld] [text "Alert Hello World!!!!"]
     ]
 
 ----------------------------------------------------------------------------
