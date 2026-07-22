1c81b7d3db75d0268a5702c64f7439476199a918
Ante Kegalj <akegalj@gmail.com>
2026-07-22 15:24:48 +0200

    Add bulma css

--- Main.hs
+++ Main.hs
@@ -8,7 +8,8 @@ module Main where
 
 ----------------------------------------------------------------------------
 import Miso
-import qualified Miso.Html as H
+import Miso.Html (body_, br_, button_, div_, doctype_, head_, html_, link_, meta_, onClick, span_, title_)
+import Miso.Html.Property (charset_, class_, classes_, content_, href_, name_, rel_)
 import Miso.Lens
 
 ----------------------------------------------------------------------------
@@ -43,11 +44,11 @@ foreign export javascript "hs_start" main :: IO ()
 
 -- | `vcomp` takes as arguments the initial model, update function, view function
 app :: App Int Action
-app =
-  (vcomp 0 updateModel viewModel)
-    { -- FIXME: use Sheet instead and convert from css into Sheet
-      styles = [Href "https://cdn.jsdelivr.net/gh/pruger/tiny-brutalism-css/tiny-brutalism.css" True]
-    }
+app = vcomp 0 updateModel viewModel
+
+--    { -- FIXME: use Sheet instead and convert from css into Sheet
+--      styles = [Href "https://cdn.jsdelivr.net/gh/pruger/tiny-brutalism-css/tiny-brutalism.css" True]
+--    }
 
 ----------------------------------------------------------------------------
 
@@ -66,11 +67,23 @@ updateModel = \case
 viewModel :: () -> Int -> View Int Action
 viewModel _props x =
   vfrag
-    [ H.button_ [H.onClick AddOne] [text "+"]
-    , text (ms x)
-    , H.button_ [H.onClick SubtractOne] [text "-"]
-    , H.br_ []
-    , H.button_ [H.onClick SayHelloWorld] [text "Alert Hello World!!!!"]
+    [ doctype_
+    , html_
+        []
+        [ head_ [] headView
+        , body_ [] bodyView
+        ]
+    ]
+ where
+  headView =
+    [ meta_ [charset_ "utf8"]
+    , meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1"]
+    , title_ [] ["Splitka"]
+    , -- FIXME: add stylesheet statically
+      link_ [rel_ "stylesheet", href_ "https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css"]
+    ]
+  bodyView =
+    [ button_ [class_ "button"] ["bok"]
     ]
 
 ----------------------------------------------------------------------------
